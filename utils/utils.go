a14763ee98b0003bcc1ec79771f0682aa034216e
bailu.wbl <bailu.wbl@alibaba-inc.com>
2020-04-17 18:02:37 +0800

    feat: echo error

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,8 @@
 package utils
 
 func ExitOnErr(err error, msg string, args ...interface{}) {
+	args = append(args, err)
 	if err != nil {
-		log.Fatalf(msg, args...)
+		log.Fatalf(msg+" , error: %v", args...)
 	}
 }
