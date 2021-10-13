dba6a44465ab786c043e99a288ea2049cdbfc778
zkcrescent <zkcrescent@gmail.com>
2021-10-13 18:54:30 +0800

    add get trace id method

--- trace/echo.go
+++ trace/echo.go
@@ -11,7 +11,7 @@ import (
 func EchoTraceMiddleWare(key string) echo.MiddlewareFunc {
 	return func(h echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			id := uuid.New()
+			id := uuid.New().String()
 			enty := logrus.WithFields(logrus.Fields{
 				TRACE_ID: id,
 			})
@@ -25,12 +25,26 @@ func EchoTraceMiddleWare(key string) echo.MiddlewareFunc {
 func GetLogger(c echo.Context) *logrus.Entry {
 	l, ok := c.Get(TRACE_LOGGER).(*logrus.Entry)
 	if !ok {
-		id := uuid.New()
-		l = logrus.WithFields(logrus.Fields{
-			TRACE_ID: id,
-		})
-		c.Set(TRACE_ID, id)
-		c.Set(TRACE_LOGGER, l)
+		_, l = setNewTrace(c)
 	}
 	return l
+}
+
+func GetTraceID(c echo.Context) string {
+	str, ok := c.Get(TRACE_ID).(string)
+	if !ok {
+		str, _ = setNewTrace(c)
+	}
+	return str
+
+}
+
+func setNewTrace(c echo.Context) (string, *logrus.Entry) {
+	id := uuid.New().String()
+	l := logrus.WithFields(logrus.Fields{
+		TRACE_ID: id,
+	})
+	c.Set(TRACE_ID, id)
+	c.Set(TRACE_LOGGER, l)
+	return id, l
 }
